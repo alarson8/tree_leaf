8d2ca1161669272a8c1440b7deb8e823d72b9fcc
Jason Chodera <jason.chodera@outlook.com>
2014-05-12 19:58:15 -0500

    session displays itself

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,10 @@
 class Session
+
   attr_accessor :start_time, :end_time, :patient_name
+
+  def display
+    "#{self.start_time} to #{self.end_time} #{self.patient_name}"
+  end
+
 end
 
--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -2,22 +2,48 @@ require 'spec_helper'
 
 describe Session do
 
-  let(:my_session) { Session.new }
+  context "displays" do
 
+    let(:my_session) { Session.new }
+    let(:sample_patient_name) { "Bob #{rand(100)}" }
+    let(:start_time) {"11:15"}
+    let(:end_time) {"11:45"}
 
-  it "has one start time" do
-    my_session.start_time = "9:00"
-    expect(my_session.start_time).to_not be_nil
+    before do
+      my_session.patient_name = sample_patient_name
+      my_session.start_time = start_time
+      my_session.end_time = end_time
+    end
+
+    it "a start time, end time, and patient name" do
+      expect(my_session.display).to eq("#{start_time} to #{end_time} #{sample_patient_name}")
+    end
+
   end
 
-  it "has one end time" do
-    my_session.end_time = "9:30"
-    expect(my_session.end_time).to eq("9:30")
-  end
+  context "attributes" do
 
-  it "has a patient name" do
-    my_session.patient_name = "Bob"
-    expect(my_session.patient_name).to eq("Bob")
+    let(:my_session) { Session.new }
+
+    before do
+      my_session.start_time = "9:00"
+      my_session.end_time = "9:30"
+      my_session.patient_name = "Bob"
+    end
+
+    it "has one start time" do
+      expect(my_session.start_time).to_not be_nil
+    end
+
+    it "has one end time" do
+      expect(my_session.end_time).to eq("9:30")
+    end
+
+    it "has a patient name" do
+      my_session.patient_name = "Bob"
+      expect(my_session.patient_name).to eq("Bob")
+    end
+
   end
 
 end
